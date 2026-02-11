22bcbae2175907df867dadba18dbaa8dd7f04217
merndev9-lang <merndev9@gmail.com>
2026-02-11 15:56:53 +0530

    done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -81,7 +81,7 @@ const Login = () => {
         <div className="text-center mb-8">
           <div className="w-20 h-20 mx-auto mb-4 rounded-full flex items-center justify-center bg-white p-2 shadow">
             <img
-              src="sks-logo.png"
+              src="sks-logo.jpg"
               alt="SKS Logo"
               className="w-full h-full object-contain"
             />
