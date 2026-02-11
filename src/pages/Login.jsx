3d03221b6fa9491275dc2513eca0cff62371dccf
merndev9-lang <merndev9@gmail.com>
2026-02-11 15:59:55 +0530

    done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -79,7 +79,7 @@ const Login = () => {
       >
         {/* Branding Section */}
         <div className="text-center mb-8">
-          <div className="w-20 h-20 mx-auto mb-4 rounded-full flex items-center justify-center bg-white p-2 shadow">
+          <div className="w-20 h-20 mx-auto mb-4 rounded-full flex items-center justify-center bg-white shadow">
             <img
               src="sks-logo.jpg"
               alt="SKS Logo"
